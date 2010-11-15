88da135f93137c6c5e30eba39dbc3d4396f4c68c
Alexey Palazhchenko <alexey.palazhchenko@qik.com>
2010-11-16 02:18:18 +0300

    Reuse socket in RubySender.

--- lib/gelf/ruby_sender.rb
+++ lib/gelf/ruby_sender.rb
@@ -2,11 +2,11 @@ module GELF
   class RubySender
     def initialize(host, port)
       @host, @port = host, port
+      @socket = UDPSocket.open
     end
 
     def send_datagrams(datagrams)
-      socket = UDPSocket.open
-      datagrams.each { |d| socket.send(d, 0, @host, @port) }
+      datagrams.each { |d| @socket.send(d, 0, @host, @port) }
     end
   end
 end

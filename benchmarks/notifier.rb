a60ab798c5603b97a2f3b1d34742adc10fd74dcd
Alexey Palazhchenko <alexey.palazhchenko@qik.com>
2010-11-16 03:01:02 +0300

    Better benchmark.

--- benchmarks/notifier.rb
+++ benchmarks/notifier.rb
@@ -5,20 +5,36 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'gelf'
 
+srand(1)
+RANDOM_DATA = ('A'..'z').to_a
+k1_message = (0..1024).map { RANDOM_DATA[rand(RANDOM_DATA.count)] }.join
+
 TARGET_HOST = 'localhost'
-TARGET_PORT = 12345
-LAN_WAN = 'LAN'
+TARGET_PORT = 12201
 DEFAULT_OPTIONS = { :host => 'localhost' }
-TIMES = 10000
-HASH = { 'short_message' => 'message' }
+TIMES = 5000
 
-notifier = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, LAN_WAN, DEFAULT_OPTIONS)
+SHORT_HASH = { 'short_message' => 'message' }
+LONG_HASH = { 'short_message' => k1_message }
 
-notifier_with_cache = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, LAN_WAN, DEFAULT_OPTIONS)
-notifier_with_cache.cache_size = 100
 
-Benchmark.bmbm do |b|
-  b.report('  no cache') { TIMES.times { notifier.notify!(HASH) } }
-  b.report('with cache') { TIMES.times { notifier_with_cache.notify!(HASH) } }
+notifier_nc_lan = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, 'LAN', DEFAULT_OPTIONS)
+notifier_nc_wan = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, 'WAN', DEFAULT_OPTIONS)
+notifier_c_lan  = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, 'LAN', DEFAULT_OPTIONS)
+notifier_c_wan  = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, 'WAN', DEFAULT_OPTIONS)
+notifier_c_lan.cache_size = notifier_c_wan.cache_size = 100
+
+puts "Sending #{TIMES} notifications..."
+tms = Benchmark.bmbm do |b|
+  b.report('lan,   no cache, short data') { TIMES.times { notifier_nc_lan.notify!(SHORT_HASH) } }
+  b.report('lan, with cache, short data') { TIMES.times { notifier_c_lan.notify!(SHORT_HASH) } }
+  b.report('wan,   no cache, short data') { TIMES.times { notifier_nc_wan.notify!(SHORT_HASH) } }
+  b.report('wan, with cache, short data') { TIMES.times { notifier_c_wan.notify!(SHORT_HASH) } }
+
+  b.report('lan,   no cache,  long data') { TIMES.times { notifier_nc_lan.notify!(LONG_HASH) } }
+  b.report('lan, with cache,  long data') { TIMES.times { notifier_c_lan.notify!(LONG_HASH) } }
+  b.report('wan,   no cache,  long data') { TIMES.times { notifier_nc_wan.notify!(LONG_HASH) } }
+  b.report('wan, with cache,  long data') { TIMES.times { notifier_c_wan.notify!(LONG_HASH) } }
 end
-raise SecurityError.new("Results are WRONG!") unless notifier_with_cache.instance_variable_get('@cache').empty?
+
+raise SecurityError.new("Results are WRONG!") unless notifier_c_lan.instance_variable_get('@cache').empty?

170d3d7ed854048cf10818e5600dd9a17ed24dd0
Alexey Palazhchenko <alexey.palazhchenko@qik.com>
2010-11-24 17:38:44 +0300

    Minor clean-up.

--- test/test_logger.rb
+++ test/test_logger.rb
@@ -10,58 +10,61 @@ class TestLogger < Test::Unit::TestCase
     end
 
     should "send pending notifications on #close" do
-      assert @notifier.respond_to?(:close)
+      @notifier.expects(:send_pending_notifications)
+      @notifier.close
     end
 
-    # logger.add(Logger::INFO, 'Message')
-    should "implement add method with level and message from parameters" do
-      @notifier.expects(:notify!).with do |hash|
-        hash['short_message'] == 'Message' &&
-        hash['level'] == GELF::INFO
+    context "#add" do
+      # logger.add(Logger::INFO, 'Message')
+      should "implement add method with level and message from parameters" do
+        @notifier.expects(:notify!).with do |hash|
+          hash['short_message'] == 'Message' &&
+          hash['level'] == GELF::INFO
+        end
+        @notifier.add(GELF::INFO, 'Message')
       end
-      @notifier.add(GELF::INFO, 'Message')
-    end
 
-    # logger.add(Logger::INFO, RuntimeError.new('Boom!'))
-    should_eventually "implement add method with level and exception from parameters"
+      # logger.add(Logger::INFO, RuntimeError.new('Boom!'))
+      should_eventually "implement add method with level and exception from parameters"
 
-    # logger.add(Logger::INFO) { 'Message' }
-    should "implement add method with level from parameter and message from block" do
-      @notifier.expects(:notify!).with do |hash|
-        hash['short_message'] == 'Message' &&
-        hash['level'] == GELF::INFO
+      # logger.add(Logger::INFO) { 'Message' }
+      should "implement add method with level from parameter and message from block" do
+        @notifier.expects(:notify!).with do |hash|
+          hash['short_message'] == 'Message' &&
+          hash['level'] == GELF::INFO
+        end
+        @notifier.add(GELF::INFO) { 'Message' }
       end
-      @notifier.add(GELF::INFO) { 'Message' }
-    end
 
-    # logger.add(Logger::INFO) { RuntimeError.new('Boom!') }
-    should_eventually "implement add method with level from parameter and exception from block"
+      # logger.add(Logger::INFO) { RuntimeError.new('Boom!') }
+      should_eventually "implement add method with level from parameter and exception from block"
 
-    # logger.add(Logger::INFO, 'Message', 'Facility')
-    should "implement add method with level, message and facility from parameters" do
-      @notifier.expects(:notify!).with do |hash|
-        hash['short_message'] == 'Message' &&
-        hash['level'] == GELF::INFO &&
-        hash['facility'] == 'Facility'
+      # logger.add(Logger::INFO, 'Message', 'Facility')
+      should "implement add method with level, message and facility from parameters" do
+        @notifier.expects(:notify!).with do |hash|
+          hash['short_message'] == 'Message' &&
+          hash['level'] == GELF::INFO &&
+          hash['facility'] == 'Facility'
+        end
+        @notifier.add(GELF::INFO, 'Message', 'Facility')
       end
-      @notifier.add(GELF::INFO, 'Message', 'Facility')
-    end
 
-    # logger.add(Logger::INFO, RuntimeError.new('Boom!'), 'Facility')
-    should_eventually "implement add method with level, exception and facility from parameters"
+      # logger.add(Logger::INFO, RuntimeError.new('Boom!'), 'Facility')
+      should_eventually "implement add method with level, exception and facility from parameters"
 
-    # logger.add(Logger::INFO, 'Facility') { 'Message' }
-    should "implement add method with level and facility from parameters and message from block" do
-      @notifier.expects(:notify!).with do |hash|
-        hash['short_message'] == 'Message' &&
-        hash['level'] == GELF::INFO &&
-        hash['facility'] == 'Facility'
+      # logger.add(Logger::INFO, 'Facility') { 'Message' }
+      should "implement add method with level and facility from parameters and message from block" do
+        @notifier.expects(:notify!).with do |hash|
+          hash['short_message'] == 'Message' &&
+          hash['level'] == GELF::INFO &&
+          hash['facility'] == 'Facility'
+        end
+        @notifier.add(GELF::INFO, 'Facility') { 'Message' }
       end
-      @notifier.add(GELF::INFO, 'Facility') { 'Message' }
-    end
 
-    # logger.add(Logger::INFO, 'Facility') { RuntimeError.new('Boom!') }
-    should_eventually "implement add method with level and facility from parameters and exception from block"
+      # logger.add(Logger::INFO, 'Facility') { RuntimeError.new('Boom!') }
+      should_eventually "implement add method with level and facility from parameters and exception from block"
+    end
 
     GELF::Levels.constants.each do |const|
       # logger.error "Argument #{ @foo } mismatch."

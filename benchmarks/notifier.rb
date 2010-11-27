34106289608db6897fe3d1db195704629ab47b80
Alexey Palazhchenko <alexey.palazhchenko@qik.com>
2010-11-27 16:43:02 +0300

    Update benchmark.

--- benchmarks/notifier.rb
+++ benchmarks/notifier.rb
@@ -1,15 +1,17 @@
 #! /usr/bin/env ruby
 
-require 'benchmark'
+puts "Loading..."
 
+require 'benchmark'
 require 'rubygems'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'gelf'
 
+puts "Generating random data..."
 srand(1)
 RANDOM_DATA = ('A'..'z').to_a
-k1_message = (0..1024).map { RANDOM_DATA[rand(RANDOM_DATA.count)] }.join
+k3_message = (1..3*1024).map { RANDOM_DATA[rand(RANDOM_DATA.count)] }.join
 
 TARGET_HOST = 'localhost'
 TARGET_PORT = 12201
@@ -17,16 +19,21 @@ DEFAULT_OPTIONS = { 'host' => 'localhost' }
 TIMES = 5000
 
 SHORT_HASH = { 'short_message' => 'message' }
-LONG_HASH = { 'short_message' => 'short message', 'long_message' => k1_message, 'user_id' => rand(10000)}
+LONG_HASH  = { 'short_message' => 'message', 'long_message' => k3_message }
 
 
 notifier_lan = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, 'LAN', DEFAULT_OPTIONS)
 notifier_wan = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, 'WAN', DEFAULT_OPTIONS)
 
-puts "Sending #{TIMES} notifications..."
-tms = Benchmark.bmbm do |b|
-  b.report('lan, short data') { TIMES.times { notifier_lan.notify!(SHORT_HASH) } }
-  b.report('wan, short data') { TIMES.times { notifier_wan.notify!(SHORT_HASH) } }
-  b.report('lan,  long data') { TIMES.times { notifier_lan.notify!(LONG_HASH) } }
-  b.report('wan,  long data') { TIMES.times { notifier_wan.notify!(LONG_HASH) } }
+# to create mongo collections, etc.
+notifier_lan.notify!(LONG_HASH)
+sleep(5)
+
+puts "Sending #{TIMES} notifications...\n"
+tms = Benchmark.bm(25) do |b|
+  b.report('lan, short data, 1 chunk ') { TIMES.times { notifier_lan.notify!(SHORT_HASH) } }
+  sleep(5)
+  b.report('lan,  long data, 1 chunk ') { TIMES.times { notifier_lan.notify!(LONG_HASH) } }
+  sleep(5)
+  b.report('wan,  long data, 2 chunks') { TIMES.times { notifier_wan.notify!(LONG_HASH) } }
 end

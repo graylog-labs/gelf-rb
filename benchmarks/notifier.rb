5b185d4cf7bc61b2398a6397c59f710ca1bbff91
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-12-03 13:21:39 +0400

    Multi-thread benchmark.

--- benchmarks/notifier.rb
+++ benchmarks/notifier.rb
@@ -1,12 +1,13 @@
 #! /usr/bin/env ruby
 
-puts "Loading..."
-
 require 'benchmark'
+require 'thread'
 require 'rubygems'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'gelf'
+
+Thread.abort_on_exception = true
 
 puts "Generating random data..."
 srand(1)
@@ -16,11 +17,21 @@ k3_message = (1..3*1024).map { RANDOM_DATA[rand(RANDOM_DATA.count)] }.join
 TARGET_HOST = 'localhost'
 TARGET_PORT = 12201
 DEFAULT_OPTIONS = { '_host' => 'localhost' }
-TIMES = 5000
+MESSAGES_COUNT = 5000
+THREADS_COUNT = 50
 
 SHORT_HASH = { 'short_message' => 'message' }
 LONG_HASH  = { 'short_message' => 'message', 'long_message' => k3_message }
 
+def notify(n, hash)
+  threads = []
+  THREADS_COUNT.times do
+    threads << Thread.new do
+      (MESSAGES_COUNT / THREADS_COUNT).times { n.notify!(hash) }
+    end
+  end
+  threads.each { |t| t.join }
+end
 
 notifier_lan = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, 'LAN', DEFAULT_OPTIONS)
 notifier_wan = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, 'WAN', DEFAULT_OPTIONS)
@@ -29,11 +40,11 @@ notifier_wan = GELF::Notifier.new(TARGET_HOST, TARGET_PORT, 'WAN', DEFAULT_OPTIO
 notifier_lan.notify!(LONG_HASH)
 sleep(5)
 
-puts "Sending #{TIMES} notifications...\n"
+puts "Sending #{MESSAGES_COUNT} notifications...\n"
 tms = Benchmark.bm(25) do |b|
-  b.report('lan, short data, 1 chunk ') { TIMES.times { notifier_lan.notify!(SHORT_HASH) } }
+  b.report('lan, short data, 1 chunk ') { notify(notifier_lan, SHORT_HASH) }
   sleep(5)
-  b.report('lan,  long data, 1 chunk ') { TIMES.times { notifier_lan.notify!(LONG_HASH) } }
+  b.report('lan,  long data, 1 chunk ') { notify(notifier_lan, LONG_HASH) }
   sleep(5)
-  b.report('wan,  long data, 2 chunks') { TIMES.times { notifier_wan.notify!(LONG_HASH) } }
+  b.report('wan,  long data, 2 chunks') { notify(notifier_wan, LONG_HASH) }
 end

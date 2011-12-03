e960e090cca91163bfe2f36e97e0e772abbce44c
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-12-03 20:29:05 +0400

    Thread-safe Ruby UDP sender.

--- lib/gelf/ruby_sender.rb
+++ lib/gelf/ruby_sender.rb
@@ -1,17 +1,31 @@
+require 'thread'
+
 module GELF
   # Plain Ruby UDP sender.
   class RubyUdpSender
-    attr_accessor :addresses
-
-    def initialize(addresses)
-      @addresses = addresses
-      @i = 0
+    def initialize(addrs)
+      @mutex = ::Mutex.new
       @socket = UDPSocket.open
+      self.addresses = addrs
+    end
+
+    def addresses
+      @addresses
+    end
+
+    def addresses=(addrs)
+      @mutex.synchronize do
+        @addresses = addrs
+        @i = 0
+        @addresses_length = addrs.length
+      end
     end
 
     def send_datagrams(datagrams)
+      # not thread-safe, but we don't care if round-robin algo fails sometimes
       host, port = @addresses[@i]
-      @i = (@i + 1) % @addresses.length
+      @i = (@i + 1) % @addresses_length
+
       datagrams.each do |datagram|
         @socket.send(datagram, 0, host, port)
       end

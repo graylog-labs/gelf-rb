f90b55dbffe6d651984c3bb146cf13d63afa2924
S. Zachariah Sprackett <zac@sprackett.com>
2014-02-08 22:54:23 -0500

    another take

--- lib/gelf/ruby_sender.rb
+++ lib/gelf/ruby_sender.rb
@@ -18,30 +18,98 @@ module GELF
     end
   end
 
+  class RubyTcpSocket
+    attr_reader :socket
+
+    def initialize(host, port)
+      @host = host
+      @port = port
+      connect
+    end
+
+    def connect
+      socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM, 0)
+      sockaddr = Socket.sockaddr_in(@port, @host)
+      begin
+        socket.connect_nonblock(sockaddr)
+      rescue Errno::EINPROGRESS, Errno::EISCONN
+      rescue Errno::EINVAL
+        return false
+      end
+      @socket = socket
+      return true
+    end
+
+    def matches?(host, port)
+      if @host == host and @port == port
+        true
+      else
+        false
+      end
+    end
+  end
+
   class RubyTcpSender
-    attr_accessor :addresses
+    attr_reader :addresses
 
     def initialize(addresses)
-      @addresses = addresses
-      @i = 0
       @sockets = []
-      @addresses.each do |address|
-        @sockets.push(TCPSocket.open(address[0], address[1]))
+      addresses.each do |address|
+        s = RubyTcpSocket.new(address[0], address[1])
+        @sockets.push(s)
+      end
+    end
+
+    def addresses=(addresses)
+      addresses.each do |address|
+        found = false
+        # handle pre existing sockets
+        @sockets.each do |socket|
+          if socket.matches?(address[0], address[1])
+            found = true
+            break
+          end
+        end
+        if not found
+          s = RubyTcpSocket.new(address[0], address[1])
+          @sockets.push(s)
+        end
       end
     end
 
     def send(message)
-      # reconnect if necessary
-      begin
-        @sockets[@i].write(message)
-      rescue Errno::ECONNRESET, Errno::EPIPE
-        # reconnect and retry once but throw exceptions on failure
-        address = addresses[@i]
-        @sockets[@i] = TCPSocket.open(address[0], address[1])
-        @sockets[@i].write(message)
-      ensure
-        @i = (@i + 1) % @addresses.length
+      while true do
+        sent = false
+        # need to only add sockets which are connected
+        sockets = @sockets.map { |s| s.socket }
+        begin
+          result = select( nil, sockets, nil, 5)
+          if result
+            writers = result[1]
+            sent = write_any(writers, message)
+          end
+          break if sent
+        rescue SystemCallError, IOError
+        end
       end
+    end
+
+    private
+    def write_any(writers, message)
+      writers.shuffle.each do |w|
+        curr_handle = w
+        begin
+          curr_handle.write(message)
+          return true
+        rescue Errno::EPIPE
+          @sockets.each do |s|
+            if s.socket == curr_handle
+              s.connect
+            end
+          end
+        end
+      end
+      return false
     end
   end
 end

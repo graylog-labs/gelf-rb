3bd9fc939fc288f9050c1d7e83a0f31fadce6b47
Bernd Ahlers <bernd@graylog.com>
2016-03-26 18:13:06 +0100

    Make udp transport thread safe
    Fixes #29

--- lib/gelf/transport/udp.rb
+++ lib/gelf/transport/udp.rb
@@ -5,20 +5,22 @@ module GELF
 
       def initialize(addresses)
         @addresses = addresses
-        @i = 0
-        @socket = UDPSocket.open
       end
 
       def send_datagrams(datagrams)
-        host, port = @addresses[@i]
-        @i = (@i + 1) % @addresses.length
+        socket = (Thread.current[:gelf_udp_socket] ||= UDPSocket.open)
+        i = (Thread.current[:gelf_udp_address_idx] ||= 0)
+
+        host, port = @addresses[i]
+        Thread.current[:gelf_udp_address_idx] = (i + 1) % @addresses.length
         datagrams.each do |datagram|
-          @socket.send(datagram, 0, host, port)
+          socket.send(datagram, 0, host, port)
         end
       end
 
       def close
-        @socket.close
+        socket = Thread.current[:gelf_udp_socket]
+        socket.close if socket
       end
     end
   end

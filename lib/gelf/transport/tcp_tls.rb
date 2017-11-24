bf38a2818b71e4b1638983c34f8fd67360751f6c
Andrey Cizov <acizov@gmail.com>
2017-11-24 15:05:11 +0000

    Fixing conditions

--- lib/gelf/transport/tcp_tls.rb
+++ lib/gelf/transport/tcp_tls.rb
@@ -107,10 +107,9 @@ module GELF
       def ssl_cert_store
         OpenSSL::X509::Store.new.tap do |store|
           if !@tls_options.key?('no_default_ca')
-            @tls_options['no_default_ca']
-          else
             @tls_options['no_default_ca'] = false
           end
+          
           if !@tls_options['no_default_ca']
             store.set_default_paths
           end
